d4749d2c1dd5d9e59d8994d5b84102f7b0afd0ef
David Fifield <david@bamsoftware.com>
2022-09-25 14:58:31 -0600

    Reduce turbotunnel queueSize from 2048 to 512.
    This is to reduce heap usage.
    
    https://gitlab.torproject.org/tpo/anti-censorship/pluggable-transports/snowflake/-/issues/40179
    
    Past discussion of queueSize:
    https://lists.torproject.org/pipermail/anti-censorship-team/2021-July/000188.html
    https://gitlab.torproject.org/tpo/anti-censorship/pluggable-transports/snowflake/-/merge_requests/48#note_2744619

--- common/turbotunnel/consts.go
+++ common/turbotunnel/consts.go
@@ -11,7 +11,7 @@ import "errors"
 var Token = [8]byte{0x12, 0x93, 0x60, 0x5d, 0x27, 0x81, 0x75, 0xf5}
 
 // The size of receive and send queues.
-const queueSize = 2048
+const queueSize = 512
 
 var errClosedPacketConn = errors.New("operation on closed connection")
 var errNotImplemented = errors.New("not implemented")
